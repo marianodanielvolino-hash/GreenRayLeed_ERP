db95d34899890a037d4dabf1e963d854edec5a14
Fito Volino <mariano.daniel.volino@gmail.com>
2026-08-13 16:49:01 -0300

    infra: add Always Free sizing variables

---
+++ infra/oci/variables-size.tf
@@ -0,0 +1,14 @@
+variable "ocpus" {
+  type    = number
+  default = 2
+}
+
+variable "memory_gb" {
+  type    = number
+  default = 12
+}
+
+variable "boot_volume_gb" {
+  type    = number
+  default = 100
+}
