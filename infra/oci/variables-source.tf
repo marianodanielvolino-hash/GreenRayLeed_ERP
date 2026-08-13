8e16904106262e9c4dd7f7fc212158ad370fb355
Fito Volino <mariano.daniel.volino@gmail.com>
2026-08-13 16:49:25 -0300

    infra: add deployment source variables

---
+++ infra/oci/variables-source.tf
@@ -0,0 +1,9 @@
+variable "git_repo_url" {
+  type    = string
+  default = "https://github.com/marianodanielvolino-hash/GreenRayLeed_ERP.git"
+}
+
+variable "git_ref" {
+  type    = string
+  default = "main"
+}
